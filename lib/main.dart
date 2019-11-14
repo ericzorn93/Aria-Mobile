f821423556f787655bad830ff25375a33afd4067
Eric Zorn <zornwebdev@gmail.com>
2019-11-14 08:09:37 -0500

    Small Title Change

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aria Music (Currently Counter)',
       theme: ThemeData(
         primaryColor: Colors.blue,
         accentColor: Colors.red,
